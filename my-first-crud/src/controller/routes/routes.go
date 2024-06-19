02034e719668e11668ba9578650677cf2ac62475
arturbaccarin <baccarin.artur@gmail.com>
2024-06-19 05:28:03 -0300

    create middleware

--- my-first-crud/src/controller/routes/routes.go
+++ my-first-crud/src/controller/routes/routes.go
@@ -2,13 +2,14 @@ package routes
 
 import (
 	"github.com/arturbaccarin/go-my-first-crud/src/controller"
+	"github.com/arturbaccarin/go-my-first-crud/src/model"
 	"github.com/gin-gonic/gin"
 )
 
 func InitRoutes(r *gin.RouterGroup, controller controller.UserControllerInterface) {
 
-	r.GET("/users/:id", controller.FindUserById)
-	r.GET("/users/emails/:email", controller.FindUserByEmail)
+	r.GET("/users/:id", model.VerifyTokenMiddleware, controller.FindUserById)
+	r.GET("/users/emails/:email", model.VerifyTokenMiddleware, controller.FindUserByEmail)
 	r.POST("/users", controller.CreateUser)
 	r.PUT("/users/:id", controller.UpdateUserById)
 	r.DELETE("/users/:id", controller.DeleteUserById)
--- my-first-crud/src/model/user_token_domain.go
+++ my-first-crud/src/model/user_token_domain.go
@@ -2,11 +2,14 @@ package model
 
 import (
 	"fmt"
+	"net/http"
 	"os"
 	"strings"
 	"time"
 
+	"github.com/arturbaccarin/go-my-first-crud/src/configuration/logger"
 	"github.com/arturbaccarin/go-my-first-crud/src/configuration/rest_err"
+	"github.com/gin-gonic/gin"
 	"github.com/golang-jwt/jwt"
 )
 
@@ -36,8 +39,9 @@ func (ud *userDomain) GenerateToken() (string, *rest_err.RestErr) {
 	return tokenString, nil
 }
 
-func VerifyToken(tokenValue string) (UserDomainInterface, *rest_err.RestErr) {
+func VerifyTokenMiddleware(c *gin.Context) {
 	secret := os.Getenv(JWT_SECRET_KEY)
+	tokenValue := RemoveBearerPrefix(c.Request.Header.Get("Authorization"))
 
 	token, err := jwt.Parse(RemoveBearerPrefix(tokenValue), func(token *jwt.Token) (interface{}, error) {
 		if _, ok := token.Method.(*jwt.SigningMethodHMAC); ok {
@@ -47,22 +51,58 @@ func VerifyToken(tokenValue string) (UserDomainInterface, *rest_err.RestErr) {
 		return nil, rest_err.NewBadRequestError("invalid token")
 	})
 	if err != nil {
-		return nil, rest_err.NewUnauthorizedRequestError("invalid token")
+		err = rest_err.NewUnauthorizedRequestError("invalid token")
+		c.JSON(http.StatusBadRequest, err)
+		c.Abort()
+		return
 	}
 
 	claims, ok := token.Claims.(jwt.MapClaims)
 	if !ok || !token.Valid {
-		return nil, rest_err.NewUnauthorizedRequestError("invalid token")
+		err = rest_err.NewUnauthorizedRequestError("invalid token")
+		c.JSON(http.StatusBadRequest, err)
+		c.Abort()
+		return
 	}
 
-	return &userDomain{
+	userDomain := &userDomain{
 		id:    claims["id"].(string),
 		email: claims["email"].(string),
 		name:  claims["name"].(string),
 		age:   int8(claims["age"].(float64)),
-	}, nil
+	}
+
+	logger.Info(fmt.Sprintf("token verified: %v", userDomain))
 }
 
+/*
+	func VerifyToken(tokenValue string) (UserDomainInterface, *rest_err.RestErr) {
+		secret := os.Getenv(JWT_SECRET_KEY)
+
+		token, err := jwt.Parse(RemoveBearerPrefix(tokenValue), func(token *jwt.Token) (interface{}, error) {
+			if _, ok := token.Method.(*jwt.SigningMethodHMAC); ok {
+				return []byte(secret), nil
+			}
+
+			return nil, rest_err.NewBadRequestError("invalid token")
+		})
+		if err != nil {
+			return nil, rest_err.NewUnauthorizedRequestError("invalid token")
+		}
+
+		claims, ok := token.Claims.(jwt.MapClaims)
+		if !ok || !token.Valid {
+			return nil, rest_err.NewUnauthorizedRequestError("invalid token")
+		}
+
+		return &userDomain{
+			id:    claims["id"].(string),
+			email: claims["email"].(string),
+			name:  claims["name"].(string),
+			age:   int8(claims["age"].(float64)),
+		}, nil
+	}
+*/
 func RemoveBearerPrefix(token string) string {
 	if strings.HasPrefix(token, "Bearer ") {
 		token = strings.TrimPrefix("Bearer ", token)
