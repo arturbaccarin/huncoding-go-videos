7b6aca34103587e99e32334ec758a9389c4f2d7a
arturbaccarin <baccarin.artur@gmail.com>
2024-08-18 17:54:32 -0300

    add commentaries

--- dsa/03-introducao-a-analise-de-algoritmos/main.go
+++ dsa/03-introducao-a-analise-de-algoritmos/main.go
@@ -2,6 +2,7 @@ package main
 
 // Não existe apenas uma forma de resolver um certo problema.
 
+// Maneira 1
 func naturalNumbers(n int) (sum int) {
 	for i := 1; i <= n; i++ {
 		sum += i
@@ -10,6 +11,7 @@ func naturalNumbers(n int) (sum int) {
 	return
 }
 
+// Maneira 2 mais eficiente
 func naturalNumbersPerf(n int) int {
 	return n * (n + 1) / 2
 }
