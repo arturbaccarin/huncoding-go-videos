66c4559c8dd55c2fb96031161891bed53374748f
arturbaccarin <baccarin.artur@gmail.com>
2024-08-28 07:30:47 -0300

    inserindo no final da lista

---
+++ dsa/33-inserindo-no-fim-da-lista/example.go
@@ -0,0 +1,57 @@
+package main
+
+import "fmt"
+
+/*
+guarda nós ligados
+sequência de nós
+nós guarda informação valor e endereço do próximo nó
+primeiro nó = head
+último nó aponta nulo
+*/
+
+type LinkedList struct {
+	Head *Node
+}
+
+func (ll *LinkedList) InsertFirst(data string) {
+	newNode := &Node{Data: data, Next: ll.Head} // o cara da primeira posição vira o da segunda posição
+	ll.Head = newNode
+}
+
+func (ll *LinkedList) InsertAtEnd(data string) {
+	newNode := &Node{Data: data, Next: ll.Head} // o cara da primeira posição vira o da segunda posição
+	if ll.Head == nil {
+		ll.Head = newNode
+		return
+	}
+
+	current := ll.Head
+	for current.Next != nil {
+		current = current.Next
+	}
+
+	current.Next = newNode
+}
+
+type Node struct {
+	Data string
+	Next *Node
+}
+
+func (n *Node) printList() {
+	current := n
+	for current != nil {
+		fmt.Print(current.Data, "->")
+		current = current.Next
+	}
+	fmt.Println("null")
+}
+
+func main() {
+	nodes := LinkedList{}
+	nodes.InsertFirst("world")
+	nodes.InsertFirst("hello")
+
+	nodes.Head.printList()
+}
