b9e415fc4a268b6998c00b4357240c3061ddac44
arturbaccarin <baccarin.artur@gmail.com>
2024-09-10 06:06:32 -0300

    remover comentários

--- dsa/65-inserindo-nos-comeco-lista/example.go
+++ dsa/65-inserindo-nos-comeco-lista/example.go
@@ -1,13 +1,5 @@
 package main
 
-/*
-Lista Ligada -> Um nó aponta para o próximo, mas não sabe quem é o nó que vem antes dele
-Lista Duplamente Ligada -> um nó aponta para o próximo e o próximo aponta para o anterior
-Lista Ligada Circular -> é uma lista ligada que o último nó aponta para o primeiro.
-
-Se só tiver um item dentro dela, ele vai apontar para ele mesmo
-*/
-
 type CircularLinkedList struct {
 	Length int
 	Last   *ListNode
