46b9afac47dd501055ab97e196a7b7d0b30d86c0
arturbaccarin <baccarin.artur@gmail.com>
2025-03-28 07:46:54 -0300

    Implement circuit breaker example with Sony's gobreaker library

---
+++ circuitbreaker/go.mod
@@ -0,0 +1,5 @@
+module cb
+
+go 1.24.0
+
+require github.com/sony/gobreaker v1.0.0
--- circuitbreaker/main.go
+++ circuitbreaker/main.go
@@ -1,3 +1,62 @@
 // https://youtu.be/7yPhLLP_Jj0
-// commit of the day
 package main
+
+import (
+	"errors"
+	"math/rand"
+	"time"
+
+	"github.com/sony/gobreaker"
+)
+
+func mockService() (string, error) {
+	if rand.Intn(100) > 50 {
+		return "success", nil
+	}
+
+	return "", errors.New("error trying to proccess request")
+}
+
+func logState(name string, from gobreaker.State, to gobreaker.State) {
+	println(name, "state changed from", stateToString(from), "to", stateToString(to))
+}
+
+func stateToString(state gobreaker.State) string {
+	switch state {
+	case gobreaker.StateClosed:
+		return "closed"
+	case gobreaker.StateOpen:
+		return "open"
+	case gobreaker.StateHalfOpen:
+		return "half-open"
+	default:
+		return "unknown"
+	}
+}
+
+func main() {
+	settings := gobreaker.Settings{
+		Name:        "my-circuitbreaker",
+		MaxRequests: 3,
+		Interval:    5 * time.Second,
+		Timeout:     5 * time.Second,
+		ReadyToTrip: func(counts gobreaker.Counts) bool {
+			return counts.ConsecutiveFailures > 2
+		},
+		OnStateChange: logState,
+	}
+
+	cb := gobreaker.NewCircuitBreaker(settings)
+
+	for i := 0; i < 10; i++ {
+		result, err := cb.Execute(func() (interface{}, error) {
+			return mockService()
+		})
+
+		if err == nil {
+			println(result.(string))
+		} else {
+			println(err.Error())
+		}
+	}
+}
