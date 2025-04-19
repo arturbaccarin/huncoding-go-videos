bd6b8aff3e6484b409a2e0c88477552a44b9568d
arturbaccarin <baccarin.artur@gmail.com>
2025-04-19 11:47:29 -0300

    slices genericos

--- novidades-go-124/main.go
+++ novidades-go-124/main.go
@@ -1,3 +1,15 @@
 // https://youtu.be/dLTpsusHkxs
 
 package main
+
+import "fmt"
+
+type Lista[T any] = []T
+
+func main() {
+	var numeros Lista[int] = []int{1, 2, 3}
+	var palavras Lista[string] = []string{"a", "b", "c"}
+
+	fmt.Println(numeros)
+	fmt.Println(palavras)
+}
