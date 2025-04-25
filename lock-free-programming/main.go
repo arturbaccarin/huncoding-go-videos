2682b7a6d7a31826654cca5a9e56aae2e6fc6df4
arturbaccarin <baccarin.artur@gmail.com>
2025-04-25 06:58:17 -0300

    start lock-free programming video

---
+++ lock-free-programming/main.go
@@ -0,0 +1,49 @@
+// https://youtu.be/8JOrxl0KkDs
+package main
+
+import (
+	"fmt"
+	"sync"
+	"sync/atomic"
+)
+
+func main() {
+	var wg sync.WaitGroup
+
+	var counter int64
+
+	for i := 0; i < 10; i++ {
+		wg.Add(1)
+		go func() {
+			defer wg.Done()
+
+			atomic.AddInt64(&counter, 1)
+		}()
+	}
+
+	wg.Wait()
+	fmt.Println(counter)
+}
+
+/*
+func main() {
+	var wg sync.WaitGroup
+	var mu sync.Mutex
+
+	counter := 0
+
+	for i := 0; i < 10; i++ {
+		wg.Add(1)
+		go func() {
+			defer wg.Done()
+
+			mu.Lock()
+			counter++
+			mu.Unlock()
+		}()
+	}
+
+	wg.Wait()
+	fmt.Println(counter)
+}
+*/
