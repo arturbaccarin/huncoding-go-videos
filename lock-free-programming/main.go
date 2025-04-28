589778c80b9d55faad438818e7762fa677a732f1
arturbaccarin <baccarin.artur@gmail.com>
2025-04-28 07:11:29 -0300

    finalizando video

--- lock-free-programming/main.go
+++ lock-free-programming/main.go
@@ -3,11 +3,96 @@
 package main
 
 import (
-	"fmt"
-	"sync"
 	"sync/atomic"
+	"unsafe"
 )
 
+type Node struct {
+	value int
+	next  *Node
+}
+
+type LockFreeQueue struct {
+	head *Node
+	tail *Node
+}
+
+func NewLockFreeQueue() *LockFreeQueue {
+	node := &Node{}
+	return &LockFreeQueue{
+		head: node,
+		tail: node,
+	}
+}
+
+func (q *LockFreeQueue) Enqueue(value int) {
+	node := &Node{value: value}
+
+	for {
+		tail := q.tail
+		next := tail.next
+
+		if atomic.CompareAndSwapPointer(
+			(*unsafe.Pointer)(unsafe.Pointer(&tail.next)),
+			unsafe.Pointer(next),
+			unsafe.Pointer(node),
+		) {
+			atomic.CompareAndSwapPointer(
+				(*unsafe.Pointer)(unsafe.Pointer(&q.tail)),
+				unsafe.Pointer(tail),
+				unsafe.Pointer(node),
+			)
+
+			return
+		}
+	}
+}
+
+func (q *LockFreeQueue) Dequeue() (int, bool) {
+	for {
+		head := q.head
+		tail := q.tail
+		next := head.next
+
+		if head == tail {
+			if next == nil {
+				return 0, false
+			}
+
+			atomic.CompareAndSwapPointer(
+				(*unsafe.Pointer)(unsafe.Pointer(&q.tail)),
+				unsafe.Pointer(tail),
+				unsafe.Pointer(next),
+			)
+		} else {
+			value := next.value
+			if atomic.CompareAndSwapPointer(
+				(*unsafe.Pointer)(unsafe.Pointer(&q.head)),
+				unsafe.Pointer(head),
+				unsafe.Pointer(next),
+			) {
+				return value, true
+			}
+		}
+	}
+}
+
+func main() {
+	queue := NewLockFreeQueue()
+
+	go queue.Enqueue(1)
+	go queue.Enqueue(2)
+	go queue.Enqueue(3)
+
+	value, ok := queue.Dequeue()
+	if ok {
+		println(value)
+	} else {
+		println("empty")
+	}
+}
+
+/*
 func main() {
 	var wg sync.WaitGroup
 
@@ -25,6 +110,7 @@ func main() {
 	wg.Wait()
 	fmt.Println(counter)
 }
+*/
 
 /*
 func main() {
