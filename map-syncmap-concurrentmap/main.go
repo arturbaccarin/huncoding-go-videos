3d2810bfb755ed5d6dd089e9ca339dc217948596
arturbaccarin <baccarin.artur@gmail.com>
2025-07-25 07:37:12 -0300

    implement standardMap function with concurrent execution

--- map-syncmap-concurrentmap/main.go
+++ map-syncmap-concurrentmap/main.go
@@ -1,2 +1,36 @@
 // https://youtu.be/yFJqQkrAj9w
 package main
+
+import (
+	"fmt"
+	"sync"
+)
+
+func standardMap() {
+	m := make(map[int]int)
+	var wg sync.WaitGroup
+
+	for i := range 10 {
+		wg.Add(1)
+		go func(i int) {
+			defer wg.Done()
+
+			m[i] = i * 10
+		}(i)
+	}
+
+	wg.Wait()
+	fmt.Println("Standard Map:", m)
+}
+
+func syncMap() {
+
+}
+
+func concurrentMap() {
+
+}
+
+func main() {
+
+}
