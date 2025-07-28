f0e33fbe97e2608827dcba71a3f7ea5243e765b4
arturbaccarin <baccarin.artur@gmail.com>
2025-07-28 06:13:05 -0300

    initialize main.go with command-line flags and add go.mod for module definition

--- raft-consensus/cmd/server/main.go
+++ raft-consensus/cmd/server/main.go
@@ -1,6 +1,33 @@
 // https://youtu.be/PJr5BT73u38
+// 12:00
 package main
+
+import (
+	"flag"
+	"log"
+	"strings"
+
+	"github.com/hashicorp/raft"
+)
 
 func main() {
 
+	nodeID := flag.String("id", "", "Node ID")
+	httpAddr := flag.String("http", ":8080", "HTTP address")
+	rpcAddr := flag.String("rpc", ":9090", "RPC address")
+	peers := flag.String("peers", "", "Comma-separated list of peer addresses")
+	flag.Parse()
+
+	if *nodeID == "" {
+		log.Fatal("Node ID is required")
+	}
+
+	// Create raft node
+	config := raft.DefaultConfig()
+	config.ID = *nodeID
+
+	// Parse peers
+	if *peers != "" {
+		config.Peers = strings.Split(*peers, ",")
+	}
 }
---
+++ raft-consensus/go.mod
@@ -0,0 +1,3 @@
+module raftconsensus
+
+go 1.24.0
